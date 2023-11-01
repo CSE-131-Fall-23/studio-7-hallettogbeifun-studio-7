6a66cf2117c71553a1717e3eea73dd2abf4f6057
jeffreyhallett <31424548+jeffreyhallett@users.noreply.github.com>
2023-11-01 17:06:44 -0500

    finished

--- src/studio7/Complex.java
+++ src/studio7/Complex.java
@@ -1,5 +1,27 @@
 package studio7;
 
 public class Complex {
-
+	
+	private int realPart;
+	private int complexPart;
+	
+	public Complex(int r, int c) {
+		this.realPart = r;
+		this.complexPart = c;
+	}
+	
+	public Complex addComplex(Complex c1, Complex c2) {
+		int newRealPart = c1.realPart + c2.realPart;
+		int newComplexPart = c1.complexPart + c2.complexPart;
+		Complex result = new Complex(newRealPart, newComplexPart);
+		return result;
+	}
+	
+	public Complex multiplyComplex(Complex c1, Complex c2) {
+		int newRealPart = (((c1.realPart) * (c2.realPart)) - ((c1.complexPart) * (c2.complexPart)));
+		int newComplexPart = (((c1.realPart) * (c2.complexPart)) + ((c1.complexPart) * (c2.realPart)));
+		Complex result = new Complex(newRealPart, newComplexPart);
+		return result;
+	}
+	
 }
--- src/studio7/HockeyPlayer.java
+++ src/studio7/HockeyPlayer.java
@@ -2,6 +2,34 @@ package studio7;
 
 public class HockeyPlayer {
 	
+	private String name;
+	private int jerseyNumber;
+	private String shootingHand;
+	private int goals;
+	private int assists;
+	private int points;
+	private int games;
+	
+	public HockeyPlayer(String n, int j, String s) {
+		this.name = n;
+		this.jerseyNumber = j;
+		this.shootingHand = s;
+		goals = 0;
+		assists = 0;
+	}
+	
+	public void recordGame(int g, int a) {
+		goals += g;
+		assists += a;
+		games++;
+	}
+	
+	public int getPoints() {
+		return assists + goals;
+	}
+	
+	
+	
 	
 
 }
